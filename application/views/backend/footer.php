5483a9c284b6302c8ca80c24d99f260d508b8197
EnderssonMendoza <enderalonso24.com>
2022-04-27 13:36:34 -0600

    Added the Application folder

---
+++ application/views/backend/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; <?php echo date('Y')?> | School Management System for "Talk academy" |
+    <strong>Developed by Endersson Mendoza</strong>
+</footer>
